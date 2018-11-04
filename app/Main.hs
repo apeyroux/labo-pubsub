48fdc998664c1a176c27d84eda850e94d3674193
Alexandre Peyroux <alex@px.io>
2018-11-04 22:23:24 +0100

    exemple with scotty

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import PubSub.Worker
+import qualified Data.ByteString.Char8 as C8
+import qualified Data.Text.Lazy as TL
+import           Database.Redis
+import           PubSub.Types
+import           PubSub.Worker
+import           Web.Scotty
 
 main :: IO ()
 main = do
+
   wid <- startWorker
+  rconn <- checkedConnect defaultConnectInfo
+
   putStrLn $ "starting " <> (show wid) <> " ..."
+
+  scotty 3000 $
+    Web.Scotty.get "/:word" $ do
+    _ <- liftAndCatchIO $ runRedis rconn $ publish "actions" (C8.pack $ show uids)
+    html $ "starting new action with " <> (TL.pack $ show uids) <> " ..."
+
   _ <- getLine
   return ()
+
+  where
+    uids :: UIds
+    uids = ["123456", "654321"]
